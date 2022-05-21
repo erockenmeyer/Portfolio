e22f7eb842eae7a477470d31ec1f83a122fa6cf6
erockenmeyer <erikrockenmeyer@gmail.com>
2022-05-20 22:45:17 -0500

    js now populates featured repos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// html ids
+const featuredEl = document.getElementById('featured');
+const notFeaturedEl = document.getElementById('not-featured');
+// choose which repos to feature, update as desired
 const featuredRepos = ['the-spell-book', 'wizards-code'];
 
 // use github api to grab my repos
@@ -46,9 +50,51 @@ const featureCheck = (name) => {
 }
 
 const displayFeaturedRepo = repo => {
-    console.log(repo);
+    // create the elements
+    const linkEl = document.createElement("a");
+    const imgEl = document.createElement("img");
+    const nameEl = document.createElement("div");
+
+    // assign classes
+    linkEl.classList = "position-relative col-auto";
+    imgEl.classList = "mw-100";
+    nameEl.classList = "site-name pl-5 pr-1 py-1"
+
+    // assign link to github repo
+    linkEl.setAttribute("href", repo.html_url);
+
+    // check which repo it is to add name, image, and alt
+    if (repo.name === "the-spell-book") {
+        // set img and alt
+        imgEl.setAttribute("src", "./assets/images/the-spell-book.png");
+        imgEl.setAttribute("alt", "a screenshot of the Spell Book website");
+
+        // add name
+        nameEl.textContent = "The Spell Book";
+    } else if (repo.name === "wizards-code") {
+        // same as above, add later
+    } else {
+        // something has gone wrong!
+        nameEl.textContent = "Oh no! If you're seeing this, something went wrong. Please let me know!"
+    }
+
+    // attach image and name to link
+    linkEl.appendChild(imgEl);
+    linkEl.appendChild(nameEl);
+
+    // attach link to featured section
+    featuredEl.appendChild(linkEl);
 }
 
 const displayRepo = repo => {
-    console.log(repo.url);
+    // very similar to featured repos, but a few changes
+    // create containers
+    const divEl = document.createElement("div");
+    const linkEl = document.createElement("a");
+    const imgEl = document.createElement("img");
+    const nameEl = document.createElement("div");
+    console.log(repo.html_url);
 }
+
+// on page load, initiate the script
+getRepos();
